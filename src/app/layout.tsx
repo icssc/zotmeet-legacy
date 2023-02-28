8d043298d4b0d0e3b9dcee86cd658fad61ead2d4
Alexander Liu <a@alexanderliu.com>
2023-02-27 19:09:56 -0800

    refactor: update typography

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,19 @@
 import "./global.scss";
 
+import { Inter } from "next/font/google";
+const inter = Inter({
+  subsets: ["latin"],
+  variable: "--font-inter",
+  display: "swap",
+});
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className={inter.className}>
       <head />
       <body>{children}</body>
     </html>

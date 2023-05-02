869c8c171fd1bc1f8f2622616617a4a72c3060f8
vatsalananthula <tkdvatsal@gmail.com>
2023-05-02 16:08:18 -0700

    allow button to accept react component

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -2,15 +2,21 @@ import cx from "classnames";
 import styles from "./button.module.scss";
 
 export interface ButtonProps {
-  label: string;
-  size?: "small" | "large";
-  color?: "sand" | "red" | "green" | "yellow" | "blue";
+  label?: string;
+  size: "small" | "large";
+  color: "sand" | "red" | "green" | "yellow" | "blue";
+  icon?: React.ReactNode;
+  invisible: boolean;
 }
 
-const Button = ({ label, size = "small", color = "sand" }: ButtonProps) => {
+const Button = ({ label, size = "small", color = "sand", icon }: ButtonProps) => {
+  const hasIcon = Boolean(icon);
+  const hasLabel = Boolean(label);
+
   return (
-    <button className={cx(styles["button"], styles[size], styles[color])}>
-      {label}
+    <button className={cx(styles["button"], styles[size], styles[color], {[styles["has-icon"]]: hasIcon, [styles["no-label"]]: !hasLabel,} )}>
+      {hasIcon && <span className={styles["icon"]}>{icon}</span>}
+      {hasLabel && label}
     </button>
   );
 };
